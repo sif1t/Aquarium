0d3dc92ce8a6a42ce1b04abe291e5bb0f5e2d177
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-11-18 20:23:23 +0600

    add queryselector video section

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 //locomotive js - smooth scrolling
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
@@ -29,6 +30,8 @@ gsap.from(".anim2",{
 
 
 
+
+
 // sheryjs to animate 
 Shery.textAnimate("#headings h1", {
     style: 2,
@@ -54,3 +57,24 @@ Shery.textAnimate("#headings h1", {
     config: {"a":{"value":0.92,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":1.8028101458680135},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.18,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.38,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}} ,
     gooey: true
   });
+
+
+
+//   add querySelector to footer section
+ document.querySelector("#ftext button")
+ .addEventListener("mouseover", function() {
+        gsap.to("#footer video", {
+            opacity: 1,
+            duration: 1,
+            ease: Power4
+        })
+ })
+ document.querySelector("#ftext button")
+ .addEventListener("mouseleave", function() {
+    gsap.to("#footer video", {
+        opacity: 0,
+        duration: 1,
+        ease: Power4
+    })
+ })
+
